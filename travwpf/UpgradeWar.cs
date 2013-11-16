70b17038187c657d155123c020eb578daf656ee0
Mizzery <angelhd@ya.ru>
2013-11-16 22:17:24 +0400

    2218

---
+++ travwpf/UpgradeWar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travwpf
+{
+    public class UpgradeWar
+    {
+        public int villnum { get; set; }
+        public bool att { get; set; }
+        public bool def { get; set; }
+
+    }
+}
